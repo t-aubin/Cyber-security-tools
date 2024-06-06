0cdc6f044c3c7165c40cffd1cbe659783683f356
t-aubin <ty.aubin@gmail.com>
2024-06-06 10:41:54 -0400

    Update haveibeenpwndsearch.py
    Add an "All" option for filtering breaches

--- haveibeenpwndsearch.py
+++ haveibeenpwndsearch.py
@@ -1,3 +1,7 @@
+# Make sure to install pandas
+# Windows: pip install pandas
+# Linux: pip3 install pandas
+
 import pandas as pd
 from tkinter import Tk, filedialog
 import tkinter as tk
@@ -36,10 +40,13 @@ def main():
             df_excel = pd.read_excel(excel_file_path)
 
             # Prompt the user for input
-            user_input = input("Enter a string to filter breaches: ")
+            user_input = input("Enter a string to filter breaches (type 'All' to include all): ")
 
             # Filter the DataFrame based on the user input
-            filtered_df_excel = df_excel[df_excel['Breaches'].str.contains(user_input, na=False)]
+            if user_input.lower() == 'all':
+                filtered_df_excel = df_excel
+            else:
+                filtered_df_excel = df_excel[df_excel['Breaches'].str.contains(user_input, na=False)]
 
             # Get the 'Email alias' values for the filtered rows
             pre_filtered_emails = filtered_df_excel['Email alias'].tolist()
